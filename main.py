59371ba10961c6eed92b6d829b6c47d82c4e36f5
Svetlana Zilpausha <70100791+SZilpausha@users.noreply.github.com>
2022-10-10 12:02:52 +0000

    3.uzdevums

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from math import*
-a=int(input("Ievadiet a:"))
-b=int(input("Ievadiet b:"))
-c=int(input("Ievadiet c:"))
-D=b**2-4*a*c
-if D<0:
-  print("Sakņu nav!")
-elif D==0:
-  x=-b/2*a
-  print("Ir viena sakne: ",x)
+sk1=int(input("Ievadiet pirmo skaitli:"))
+sk2=int(input("Ievadiet otro skaitli:"))
+oper=input("Ievadiet matemātisko operāciju:")
+if oper=="+":
+  print("Summa =",sk1+sk2)
+elif oper=="-":
+  print("Atņemšanas rezultāts =",sk1-sk2)
+elif oper=="*":
+  print("Reizinājums =",sk1*sk2)
+elif oper=="/":
+  print("Dalījums =",sk1/sk2)
+elif oper=="s":
+  print("Kvadrātsakne =",sqrt(sk1))
 else:
-  x1=(-b+sqrt(D))/2*a
-  x2=(-b-sqrt(D))/2*a
-  print("Ir divas saknes, x1 = ",x1," x2 =",x2)
+  print("Šaja matemātiskā operācija netiek atbastīta!!!")
