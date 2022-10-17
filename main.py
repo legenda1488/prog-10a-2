9e7600573a433797376831c206e169ebb864a88c
Nikita <114918709+legenda1488@users.noreply.github.com>
2022-10-17 11:57:42 +0000

    4 uzd

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-print("ievadi 3 leņķus")
-a=int(input("Ievadiet 1. leņķ: "))
-b=int(input("Ievadiet 2. leņķ: "))
-c=int(input("Ievadiet 3. leņķ: "))
-t=a+b+c
-if t>180:
-  print("tristuris neeksiste")
-if t<180:
-  print("tristuris neeksiste")
-elif a+b+c==180:
-  print("tristuris eksiste")
-if a==b==c: print("tas ir vienadmalu tristuris")
-elif a==90: print("tas ir taisnleņķa trijstūris")
-elif b==90: print("tas ir taisnleņķa trijstūris")
-elif c==90: print("tas ir taisnleņķa trijstūris")
-if a>90: print("tas ir platleņķa trijstūris")
-elif b>90: print("tas ir platleņķa trijstūris")
-elif c>90: print("tas ir platleņķa trijstūris")
-if a<90 and b<90 and c<90: print("tas ir šaurleņķa trijstūris"
+sk=int(input("введите число "))
+if sk<0:
+  print("это негативное число")
+if sk>0:
+  print("это позитивное число")
+if sk<-999: print("четырёх(или более)значное число")
+elif sk<-99: print("трёхзначное число")
+elif sk<-9: print("двухзначное число")
+elif sk>999: print("четырёх(или более)значное число")
+elif sk>99: print("трёхзначное число")
+elif sk>9: print("двухзначное число")
+elif sk>0 and sk<10 and sk>-10: print("однозначное число")
